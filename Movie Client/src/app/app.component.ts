5c8865967b9258e1c6dece481f8ca4d9abd90338
Padmavathi Rudra <48172599+padhurudra@users.noreply.github.com>
2019-06-06 19:03:46 -0400

    Add files via upload

---
+++ Movie Client/src/app/app.component.ts
@@ -0,0 +1,10 @@
+import { Component, OnInit, getModuleFactory } from '@angular/core';
+@Component({
+  selector: 'app-root',
+  templateUrl: './app.component.html',
+})
+export class AppComponent implements OnInit {
+  title = 'Movie Application';
+  ngOnInit() {
+  }
+}
---
+++ Movie Client/src/app/login/login.component.ts
@@ -0,0 +1,36 @@
+import { Component, OnInit } from '@angular/core';
+import { ActivatedRoute, Router } from "@angular/router";
+import { MovieService } from '../movie.service';
+import { AuthService } from '../auth.service';
+@Component({
+  selector: 'app-login',
+  templateUrl: './login.component.html',
+})
+export class LoginComponent implements OnInit {
+iserror: boolean;
+  constructor(
+    private router: Router,
+  private movieService: MovieService,private AuthService:AuthService)
+             {}
+    ngOnInit() {
+  }
+  login(data:any) {
+var registration = {
+"userName":data.userName,
+"Password":data.password
+}
+this.AuthService.login(data)
+.subscribe(
+(data) => {
+if(data){
+localStorage.setItem('access_token',data.token);
+  console.log(data.token);
+  this.router.navigate(['/movies']);
+}
+});
+if(data.usern==null)
+{
+  this.iserror=true;
+}
+}
+}
